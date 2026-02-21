856b77959f790ddd4bb9ce94fa4d52f5ba2f552f
Vladcom4iiik <solovyev4625@gmail.com>
2026-02-21 20:31:07 +0300

    фикс багов

--- threadly-frontend/src/profile/Profile.js
+++ threadly-frontend/src/profile/Profile.js
@@ -214,6 +214,7 @@ const Profile = (props) => {
                             },
                         ]}
                     >
+                        <Input placeholder="username"/>
                     </Form.Item>
 
                     <Form.Item
